141dfb1b3b888186aa05f397a7a06d21beaf95f2
claudio pizarro <claudeus123>
2023-06-26 19:43:34 -0400

    automatization removed

--- src/modules/automatization/automatization.service.ts
+++ src/modules/automatization/automatization.service.ts
@@ -11,24 +11,24 @@ export class AutomatizationService {
         @InjectRepository(Reserve) private reserveRepository: Repository<Reserve>
     ){}
 
-    @Cron('0 * * * *') 
-    async updateReserves(): Promise<void> {
-        const reserves = await this.reserveRepository.find();
-        // console.log(reserves[0].end_time.replace(':',''));
-        const actualTime = new Date().toString().split(' ')[4].split(':')[0] + new Date().toString().split(' ')[4].split(':')[1]
-        // console.log(actualTime)
-        reserves.forEach((reserve) => {
-            if (!isEqual(new Date(), new Date(reserve.start_date)) && reserve.end_date === null) reserve.available = true;
-            // if (reserve.end_date === null && isEqual(new Date(), new Date(reserve.start_date)) && reserve.end_time.replace(':','') < actualTime) reserve.available = true;
-            // if(reserve.end_date !== null && isAfter(new Date(), new Date(reserve.end_date))) reserve.available = true;
-            if(reserve.end_date !== null && isAfter(new Date(), new Date(reserve.end_date)) || (reserve.end_date !== null && isEqual(new Date(), new Date(reserve.end_date)) && reserve.end_time.replace(':','') < actualTime)) reserve.available = true;
-            // console.log(reserve.end_date);
-            // console.log(isEqual(new Date(), new Date(reserve.start_date)))
-            // console.log(reserve.end_time.replace(':','') < actualTime)
-            console.log(reserve.end_date === null);
-        });
+    // @Cron('0 * * * *') 
+    // async updateReserves(): Promise<void> {
+    //     const reserves = await this.reserveRepository.find();
+    //     // console.log(reserves[0].end_time.replace(':',''));
+    //     const actualTime = new Date().toString().split(' ')[4].split(':')[0] + new Date().toString().split(' ')[4].split(':')[1]
+    //     // console.log(actualTime)
+    //     reserves.forEach((reserve) => {
+    //         if (!isEqual(new Date(), new Date(reserve.start_date)) && reserve.end_date === null) reserve.available = true;
+    //         // if (reserve.end_date === null && isEqual(new Date(), new Date(reserve.start_date)) && reserve.end_time.replace(':','') < actualTime) reserve.available = true;
+    //         // if(reserve.end_date !== null && isAfter(new Date(), new Date(reserve.end_date))) reserve.available = true;
+    //         if(reserve.end_date !== null && isAfter(new Date(), new Date(reserve.end_date)) || (reserve.end_date !== null && isEqual(new Date(), new Date(reserve.end_date)) && reserve.end_time.replace(':','') < actualTime)) reserve.available = true;
+    //         // console.log(reserve.end_date);
+    //         // console.log(isEqual(new Date(), new Date(reserve.start_date)))
+    //         // console.log(reserve.end_time.replace(':','') < actualTime)
+    //         console.log(reserve.end_date === null);
+    //     });
 
-        console.log(reserves);
-    }
+    //     console.log(reserves);
+    // }
 
 }
