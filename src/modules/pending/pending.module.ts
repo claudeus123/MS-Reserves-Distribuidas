50bc0eabe9b0a3e06eed8500e94cd005353fa672
claudio pizarro <claudeus123>
2023-06-26 13:38:44 -0400

    pending bug solved

--- src/modules/pending/pending.module.ts
+++ src/modules/pending/pending.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PendingService } from './pending.service';
 import { PendingController } from './pending.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Reserve } from 'src/entities/reserves.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Reserve])],
   controllers: [PendingController],
   providers: [PendingService]
 })
